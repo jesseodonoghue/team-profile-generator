82d48af302c56dfc04e9dba66223a90243f6ff3d
Jesse O'Donoghue <jesseodonoghue@gmail.com>
2020-08-31 00:28:43 -0400

    fix commenting

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const render = require("./lib/htmlRenderer");
 const { type } = require("os");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
 const team = []; // empty array for team members
 
@@ -140,7 +138,7 @@ createEmployee = (name, id, email, role) => {
             break;
     }
 
-}
+} // Function to create a new instance of an employee type
 
 writeToFile = (fileName, data) => {
     fs.writeFile(fileName, data, (err) => {
@@ -150,7 +148,7 @@ writeToFile = (fileName, data) => {
 
         console.log("HTML file successfully generated!!");
     });
-}
+} // Function to write a file to disk
 
 init = async () => {
 
@@ -195,30 +193,7 @@ init = async () => {
     const output = render(team);
     writeToFile(outputPath, output);
 
-}
+} // Main program function
 
 
-
-init();
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+init(); // Execute main program function
